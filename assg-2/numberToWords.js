5053606fbedf18a67d418853f96fa17bf2f64735
Saran <saranrajmalar@gmail.com>
2016-11-21 23:06:04 +0530

    Saran | delete commented lines

--- assg-2/numberToWords.js
+++ assg-2/numberToWords.js
@@ -8,13 +8,9 @@ let operators = {
 };
 
 const parser = function (tree) {
-    // let leftWord = inWord(tree.left);
-    // let rightWord = inWord(tree.right);
+
     let symWord = operators[tree.sym];
 
-    if (!tree.left.sym && !tree.right.sym){
-        return `(${inWord(tree.left)} ${symWord} ${inWord(tree.right)})`;
-    }
     if (tree.left.sym && tree.right.sym){
         return `(${parser(tree.left)} ${symWord} ${parser(tree.right)})`;
     }
@@ -24,15 +20,7 @@ const parser = function (tree) {
     if (!tree.left.sym && tree.right.sym){
         return `(${inWord(tree.left)} ${symWord} ${parser(tree.right)})`;
     }
-
-    // if (typeof tree.left != 'number') {
-    //     tree.left = parser(tree.left);
-    // }
-    // if (typeof tree.right != 'number') {
-    //     tree.right = parser(tree.right);
-    // }
-    //
-    // return `${leftWord} ${symWord} ${rightWord}`;
+    return `(${inWord(tree.left)} ${symWord} ${inWord(tree.right)})`;
 };
 
 exports.parse = parser;
